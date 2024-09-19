65f26dfe14667c9872dfe4c81fb29908b5a06121
wildevn <wiledu@hotmail.com>
2024-09-18 23:30:35 -0400

    Same as last one.

--- src/models/schema.ts
+++ src/models/schema.ts
@@ -1,12 +1,21 @@
-import { mysqlTable, text, int, date, varchar, tinyint, decimal, mediumtext } from 'drizzle-orm/mysql-core';
+import {
+  mysqlTable,
+  text,
+  int,
+  date,
+  varchar,
+  tinyint,
+  decimal,
+  mediumtext,
+} from "drizzle-orm/mysql-core";
 
-export const Users = mysqlTable('Users', {
-  id: int('id').notNull().autoincrement().primaryKey(),
-  name: varchar('name', { length: 255 }),
-  email: varchar('email', { length: 255 }).notNull(),
-  password: varchar('password', { length: 255 }).notNull(),
-  createdAt: date('createdAt').notNull(),
-  updatedAt: date('updatedAt').notNull(),
+export const Users = mysqlTable("Users", {
+  id: int("id").notNull().autoincrement().primaryKey(),
+  name: varchar("name", { length: 255 }),
+  email: varchar("email", { length: 255 }).notNull(),
+  password: varchar("password", { length: 255 }).notNull(),
+  createdAt: date("createdAt").notNull(),
+  updatedAt: date("updatedAt").notNull(),
 });
 
 export const Locales = mysqlTable("Locales", {
@@ -25,7 +34,10 @@ export const Locales = mysqlTable("Locales", {
 });
 
 export const ScheduledHours = mysqlTable("ScheduledHours", {
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(), // TODO: verify the foreign key
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(), // TODO: verify the foreign key
   sundayHours: varchar("sundayHours", { length: 20 }),
   mondayHours: varchar("mondayHours", { length: 20 }),
   tuesdayHours: varchar("tuesdayHours", { length: 20 }),
@@ -39,62 +51,95 @@ export const ScheduledHours = mysqlTable("ScheduledHours", {
 
 export const Photos = mysqlTable("Photos", {
   id: int("id").notNull().autoincrement().primaryKey(),
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   name: varchar("name", { length: 255 }).notNull(),
   data: mediumtext("data").notNull(),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
 
 export const Favorites = mysqlTable("Favorites", {
-  userId: int("userId").notNull(),
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  userId: int("userId")
+    .notNull()
+    .references(() => Users.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
 
 export const Histories = mysqlTable("Histories", {
-  userId: int("userId").notNull(),
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  userId: int("userId")
+    .notNull()
+    .references(() => Users.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
 
 export const Reviews = mysqlTable("Reviews", {
-  userId: int("userId").notNull().references(() => Users.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  userId: int("userId")
+    .notNull()
+    .references(() => Users.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   grade: decimal("grade").notNull().default("0.0"),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
 
 export const AcademicBlocks = mysqlTable("AcademicBlocks", {
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   course: varchar("course", { length: 30 }).notNull(),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
 
 export const Libraries = mysqlTable("Libraries", {
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   libraryLink: varchar("libraryLink", { length: 30 }).notNull(),
   rules: text("rules"),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
 
 export const Sports = mysqlTable("Sports", {
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   availableSports: varchar("availableSports", { length: 255 }).notNull(),
   rules: text("rules"),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
 
 export const Transports = mysqlTable("Transports", {
-  localeId: int("localeId").notNull().references(() => Locales.id, { onDelete: 'cascade', onUpdate: 'cascade' }).unique(),
+  localeId: int("localeId")
+    .notNull()
+    .references(() => Locales.id, { onDelete: "cascade", onUpdate: "cascade" })
+    .unique(),
   availableBuses: varchar("availableBuses", { length: 30 }).notNull(),
   rules: text("rules"),
   createdAt: date("createdAt").notNull(),
   updatedAt: date("updatedAt").notNull(),
-})
+});
