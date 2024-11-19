be0f218c8920657bbbef077fb56df060aef56b58
wildevn <wiledu@hotmail.com>
2024-11-18 20:23:13 -0400

    Now is possible to update photos of a locale

--- src/services/localesServices/updateLocaleService.ts
+++ src/services/localesServices/updateLocaleService.ts
@@ -12,7 +12,13 @@ import { eq } from "drizzle-orm";
 import type { ResultAction } from "./createOrUpdateReviewService";
 import { scheduleAttributes } from "./showLocalesService";
 import type { Hours } from "./listSectionService";
+import fs from "node:fs";
 
+type Photo = {
+  id?: number;
+  name?: string;
+  data?: string;
+};
 export type EditLocale = {
   name?: string;
   localizationLink?: string;
@@ -25,11 +31,7 @@ export type EditLocale = {
   phoneNumber?: string;
   accessibility?: number;
   updatedAt: Date;
-  photos?: Array<{
-    id: number;
-    name: string;
-    data: string;
-  }>;
+  photos?: Array<Photo>;
   schedule?: {
     monday: string;
     tuesday: string;
@@ -221,6 +223,53 @@ const updateLocaleService = async (
       }
       if (photos) {
         // search and add photos
+        try {
+          const currentLocalePhotosIds: Photo[] = await db
+            .select({ id: Photos.id })
+            .from(Photos)
+            .where(eq(Photos.localeId, localeId));
+          if (currentLocalePhotosIds?.length > 0) {
+            for (const photoId of currentLocalePhotosIds) {
+              if (photos.findIndex((photo) => photo.id === photoId.id) === -1) {
+                await db.delete(Photos).where(eq(Photos.id, photoId.id || 0));
+              }
+            }
+          }
+
+          for (const photo of photos) {
+            try {
+              if (photo.id) {
+                continue;
+              }
+              if (photo.data) {
+                const newPhotoUrl = `/public/${date.getTime()}_${photo.name}`;
+                fs.writeFileSync(`.${newPhotoUrl}`, photo.data, {
+                  encoding: "base64",
+                });
+
+                const photoId = (
+                  await db
+                    .insert(Photos)
+                    .values({
+                      localeId: localeId,
+                      name: photo?.name || "photo",
+                      url: newPhotoUrl,
+                      createdAt: date,
+                      updatedAt: date,
+                    })
+                    .$returningId()
+                )[0];
+              }
+            } catch (err) {
+              result.photos = "failed to update";
+            }
+          }
+          result.photos = "updated";
+        } catch (err) {
+          if (err) {
+            result.photos = "failed to update";
+          }
+        }
       }
       if (schedule) {
         try {
